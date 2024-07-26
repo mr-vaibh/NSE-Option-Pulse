4b5f7243fabc4f40adc72b98f4ca19c179e1181d
Vaibhav Shukla <shuklavaibhav336@gmail.com>
2024-07-26 17:29:16 +0530

    default port to 80

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def stop_script():
     return jsonify({'status': 'Script stopped successfully', 'pid': pid}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
